23de6d38c4eb73815eb31844caff2234bd33ffb2
wolfwithcode <wolfwithcode@gmail.com>
2020-06-10 16:13:58 +0700

    Implement catching timestamp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { subscribeToTimer } from './api';
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    subscribeToTimer((timestamp) => {
+      this.setState({
+        timestamp
+      });
+    });
+  }
+
+  state = {
+    timestamp: 'no timestamp yet',
+  }
 
   render() {
     return (
@@ -9,7 +23,7 @@ class App extends Component {
         <div className="App-header">
           <h2>Our awesome drawing app</h2>
         </div>
-        hello
+        This is the value of the timer timestamp: {this.state.timestamp}
       </div>
     );
   }
