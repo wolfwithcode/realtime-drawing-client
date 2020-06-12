5b28d6338fbaa7277bc926b257646612852a8959
anhld <wolfwithcode@gmail.com>
2020-06-12 16:10:27 +0700

    Implement publish drawing function

--- src/Drawing.js
+++ src/Drawing.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import Canvas from 'simple-react-canvas';
+import { publishLine } from './api'
 
 export default class Drawing extends Component {
+    handleDraw = (line) => {
+        publishLine({
+            drawingId: this.props.drawing.id,
+            line,
+        });
+    }
+
     render() {
         return (this.props.drawing) ? (
             <div
                 className="Drawing"
             >
                 <div className="Drawing-title">{this.props.drawing.name}</div>
-                <Canvas drawingEnable={true} />
+                <Canvas 
+                    drawingEnable={true} 
+                    onDraw={this.handleDraw}
+                />
             </div>
         ) : null;
     }
